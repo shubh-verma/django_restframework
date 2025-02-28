09983b241eab93188135836490fbdc8c2aa977e1
shubh-verma <shubhvcs@gmail.com>
2025-02-28 11:54:40 +0530

    [ADD] T4: Auth & Per User level

--- tutorials/urls.py
+++ tutorials/urls.py
@@ -19,6 +19,7 @@ from rest_framework import routers
 
 from tutorial.quickstart import views
 
+
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
